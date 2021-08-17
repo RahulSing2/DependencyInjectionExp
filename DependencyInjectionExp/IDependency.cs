fb5f8a05c9eec9f3c96feb29a1fdd96291796a96
RahulSing2 <88368239+RahulSing2@users.noreply.github.com>
2021-08-17 16:25:04 +0530

    DependencyInjectionExp Demo

---
+++ DependencyInjectionExp/IDependency.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionExp
+{
+   public interface IDependency
+    {
+       public string  WriteMessage(string message);
+    }
+}
---
+++ DependencyInjectionExp/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionExp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MyDependency _dependency = new MyDependency();
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult OnGet()
+        {
+            string strMessage = _dependency.WriteMessage("IndexModel.OnGet created this information.");
+            return Content(strMessage);
+        }
+    }
+}
